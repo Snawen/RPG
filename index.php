1c86c0c240f912e19491cbc93bdf830d579bdd9a
Snawen <axel.isaia84@gmail.com>
2020-03-30 15:53:23 +0200

    Exercice 3

--- index.php
+++ index.php
@@ -2,41 +2,86 @@
 
 //Caractéristique des personnages 
 class Personnage {
-    public $nom;
-    public $force;
-    public $niveau;
-    public $sante;
-    public $mort;
+    private $nom;
+    private $force;
+    private $niveau;
+    private $sante;
+    private $mort;
 
     public function caracteristique () {
         $etat = ($this->mort)? "mort" : "vivant";
         echo $this->nom ." a une force de ".$this->force." et est au niveau ".$this->level. ", sont état de santé est de: ".$this->sante." points  sur 100, votre héro est donc ".$etat;
         }
+
+        function getNom(): string {
+            return $this->nom;
+        }
+
+        function setNom(string $nom) {
+            $this->nom = $nom;
+        }
+
+        function getForce(): int {
+            return $this->force;
+        }
+
+        function setForce(int $force) {
+            $this->force = $force;
+        }
+
+        function getLevel(): int {
+            return $this->level;
+        }
+
+        function setLevel(int $level) {
+            $this->level = $level;
+        }
+
+        function getSante(): int {
+            return $this->sante;
+        }
+
+        function setSante(int $sante) {
+            $this->sante = $sante;
+            if($this ->sante < 1) {
+                $this->setMort(true);
+            }else {
+                $this->setMort(false);
+            }
+        }
+
+        function getMort(): bool {
+            return $this->mort;
+        }
+
+        function setMort(int $mort) {
+            $this->mort = $mort;
+        }
 }
 
 //Fiche des persos
 $perso1 = new Personnage();
-$perso1->nom = "Grüdu";
-$perso1->force= 15;
-$perso1->level = 1;
-$perso1->sante = 100;
-$perso1->mort = false;
+$perso1->setNom("Grüdu");
+$perso1->setForce(15);
+$perso1->setLevel(1);
+$perso1->setSante(100);
+
 
 
 $perso2 = new Personnage();
-$perso2->nom = "Snawen";
-$perso2->force= 10;
-$perso2->level = 1;
-$perso2->sante = 75;
-$perso2->mort = false;
+$perso2->setNom("Snawen");
+$perso2->SetForce(10);
+$perso2->SetLevel(1);
+$perso2->SetSante(75);
+
 
 
 $perso3 = new Personnage();
-$perso3->nom = "Jean-Clude";
-$perso3->force= 14;
-$perso3->level = 1;
-$perso3->sante = 0;
-$perso3->mort = true;
+$perso3->setNom("Jean-Clude");
+$perso3->setForce(14);
+$perso3->setLevel(1);
+$perso3->setSante(0);
+
 
 
 $perso1->caracteristique();
